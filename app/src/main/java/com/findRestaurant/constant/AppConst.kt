78884e7bb314d9fcd2557e21f8a6ad79dbf33a7f
Bhadresh <bhadresh.chavada@octosglobal.info>
2020-05-13 11:48:29 +0530

    init commit

--- app/src/main/java/com/findRestaurant/constant/AppConst.kt
+++ app/src/main/java/com/findRestaurant/constant/AppConst.kt
@@ -1,21 +1,10 @@
 package com.findRestaurant.constant
 
-import com.findRestaurant.BuildConfig
-
 object AppConst {
     fun BASE_URL(): String {
-        if (BuildConfig.FLAVOR.equals("dev")) {
-//            return "https://dev.octosglobal.info/hopp/"
-            return "http://opentable.herokuapp.com/api/"
-        } else if (BuildConfig.FLAVOR.equals("qa")) {
-            return "http://opentable.herokuapp.com/api/"
-        } else if (BuildConfig.FLAVOR.equals("prod")) {
-            return "http://opentable.herokuapp.com/api/"
-        } else if (BuildConfig.FLAVOR.equals("stg")) {
-            return "http://opentable.herokuapp.com/api/"
-        } else {
-            return "http://opentable.herokuapp.com/api/"
-        }
+
+        return "http://opentable.herokuapp.com/api/"
+
 
     }
 
